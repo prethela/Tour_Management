3b14a5d088c8e702d38cae46f953d973ae4466ce
Shah Nusrat Unzom Prethela <nusratunzomprethela@gmail.com>
2026-07-09 23:41:21 +0600

    Update Payment.java

--- Payment.java
+++ Payment.java
@@ -1,1 +1,22 @@
 
+
+package TourManagement;
+// File: Payment.java
+public class Payment {
+    public static void processPayment(String method, double amount) {
+        System.out.println("\nPayment Method: " + method);
+        if (method.equalsIgnoreCase("credit")) {
+            System.out.println("Processing credit card payment of Tk " + amount + "...");
+        } else {
+            System.out.println("Processing cash payment of Tk " + amount + "...");
+        }
+
+        try {
+            Thread.sleep(2000); // simulate processing time
+        } catch (InterruptedException e) {
+            System.out.println("Payment interrupted!");
+        }
+
+        System.out.println("Payment successful! Thank you.");
+    }
+}
