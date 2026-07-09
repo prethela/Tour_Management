1e2b3e03babd307e44c6ad22a0c324fc48b76bfd
Shah Nusrat Unzom Prethela <nusratunzomprethela@gmail.com>
2026-07-09 22:05:30 +0600

    Update TripThread.java

--- TripThread.java
+++ TripThread.java
@@ -1,1 +1,24 @@
 
+package TourManagement;
+// File: TripThread.java
+public class TripThread extends Thread {
+    private String destination;
+
+    public TripThread(String destination) {
+        this.destination = destination;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("\nPreparing your trip to " + destination + "...");
+        try {
+            for (int i = 1; i <= 3; i++) {
+                System.out.println("Loading step " + i + "...");
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("️ Preparation interrupted.");
+        }
+        System.out.println(" Trip to " + destination + " is ready!");
+    }
+}
