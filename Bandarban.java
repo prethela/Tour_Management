82717c1bf406ee736a46c40cf2d9053798abdd15
Shah Nusrat Unzom Prethela <nusratunzomprethela@gmail.com>
2026-07-10 00:07:23 +0600

    Update Bandarban.java

--- Bandarban.java
+++ Bandarban.java
@@ -1,1 +1,22 @@
 
+
+package TourManagement;
+// File: Bandarban.java
+public class Bandarban extends Tour {
+    public Bandarban() {
+        super("Bandarban", 4, "Evening", "2 meals/day", "Resort", 
+              "July 1 - July 10", "Bus", 9500.0);
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("=== Bandarban Tour Details ===");
+        System.out.println("Duration: " + duration + " days");
+        System.out.println("Time: " + time);
+        System.out.println("Meals: " + meals);
+        System.out.println("Accommodation: " + accommodation);
+        System.out.println("Available Dates: " + availableDates);
+        System.out.println("Transport: " + transport);
+        System.out.println("Price: Tk " + price);
+    }
+}
